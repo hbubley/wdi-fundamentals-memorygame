3118f3d95640bdc3b5299dab855fefbbf86bbfec
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-02-13 10:54:00 -0600

    created game logic

---
+++ memory_game/JS/main.js
@@ -0,0 +1,9 @@
+console.log("Up and RUNNING")
+
+let cardOne = "queen";
+let cardTwo = "king";
+let cardThree = "king";
+let cardFour = "queen";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
