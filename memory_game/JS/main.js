68c8773ec2cf32bdba827d6bc1f4fe5eb009e8b9
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-02-14 08:28:01 -0600

    Created JS arrays, game logic.

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,9 +1,22 @@
 console.log("Up and RUNNING")
 
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "king";
-let cardFour = "queen";
+const cards = ["King", "King", "Queen", "Queen"];
+const cardsPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+let cardOne = cards[1];
+console.log("User just flipped " + cardOne)
+let cardTwo = cards[2];
+console.log("User just flipped " + cardTwo)
+cardsPlay.push(cardOne);
+cardsPlay.push(cardTwo);
+
+if(cardsPlay.length === 2)
+{
+  if(cardsPlay[0] === cardsPlay[1])
+  {
+    alert("You found a match!");
+  }
+  else {
+    alert("Sorry, try again!")
+  }
+}
