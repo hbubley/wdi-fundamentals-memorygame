b07afbbd2b27f77581fd45f84141cc952f982d94
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-02-15 07:41:07 -0600

    Changed the array 'cards' to an object array.

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,6 +1,28 @@
 console.log("Up and RUNNING")
 
-const cards = ["King", "King", "Queen", "Queen"];
+const cards =
+[
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png"
+  }
+];
 const cardsInPlay = [];
 
 function checkForMatch(cardsInPlay)
@@ -19,9 +41,11 @@ function checkForMatch(cardsInPlay)
 
 function flipCard(cardID)
 {
-  cardsInPlay.push(cards[cardID]);
-  console.log("User just flipped " + cards[cardID]);
-  checkForMatch(cardsInPlay)
+  cardsInPlay.push(cards[cardID].rank);
+  console.log("User just flipped " + cards[cardID].rank);
+  console.log(cards[cardID].suit);
+  console.log(cards[cardID].cardImage);
+  checkForMatch(cardsInPlay);
 }
 
 
