8710eae247190cc168af27b21036f6794db9b7b1
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-02-16 13:43:11 -0600

    Added javascript card flipping functionality.

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -5,50 +5,65 @@ const cards =
   {
     rank: "queen",
     suit: "hearts",
-    cardImage: "images/queen-of-hearts.png"
+    cardImage: "IMG/queen-of-hearts.png"
   },
   {
     rank: "queen",
     suit: "diamonds",
-    cardImage: "images/queen-of-diamonds.png"
+    cardImage: "IMG/queen-of-diamonds.png"
   },
   {
     rank: "king",
     suit: "hearts",
-    cardImage: "images/king-of-hearts.png"
+    cardImage: "IMG/king-of-hearts.png"
   },
   {
-  rank: "king",
-  suit: "diamonds",
-  cardImage: "images/king-of-diamonds.png"
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "IMG/king-of-diamonds.png"
   }
 ];
 const cardsInPlay = [];
 
 function checkForMatch(cardsInPlay)
 {
-  if (cardsInPlay.length===2)
-  {
     if(cardsInPlay[0] === cardsInPlay[1])
     {
-      console.log("You found a match!");
+      alert("You found a match!");
     }
     else {
-      console.log("Sorry, try again!");
+      alert("Sorry, try again!");
     }
-  }
 }
 
-function flipCard(cardID)
+function flipCard()
 {
+  cardID = this.getAttribute('data-id');
   cardsInPlay.push(cards[cardID].rank);
   console.log("User just flipped " + cards[cardID].rank);
   console.log(cards[cardID].suit);
   console.log(cards[cardID].cardImage);
-  checkForMatch(cardsInPlay);
+  this.setAttribute('src', cards[cardID].cardImage);
+  if (cardsInPlay.length===2)
+  {
+    checkForMatch(cardsInPlay);
+  }
+  else if (cardsInPlay.length>2)
+  {
+    this.addEventListener('click', alert("You have already picked two cards!"));
+  }
 }
 
+function createBoard()
+{
+  for(let i = 0; i < cards.length; i++)
+  {
+    let cardElement = document.createElement('img');
+    cardElement.setAttribute('src', "IMG/back.png");
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
 
-
-flipCard(0);
-flipCard(2);
+createBoard();
